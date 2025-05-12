ec7b71f8c85003ad9b28c554cdf3f37e5cc28248
Diarah-zr <diarahtraore78@gmail.com>
2025-05-12 16:39:07 +0200

    php css

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
+
 <?php
-include_once __DIR__ ""
+include_once __DIR__ ."/controller/BaseController.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
---
+++ pages/home.php
@@ -0,0 +1,6 @@
+<section>
+    <h2>Bienvenue sur la page d'accueil 🚀​</h2>
+    <p>
+        Ceci est la page d'accueil de notre site.
+    </p>
+</section>
